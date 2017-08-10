ca3902a3ce890d39d578cc58a50e254d18164ddb
Tare Gaskin <tare.dgg@gmail.com>
2017-08-10 12:03:42 -0400

    aave

--- IRP_Hand/IRP_Hand/spiders/test_spider.py
+++ IRP_Hand/IRP_Hand/spiders/test_spider.py
@@ -10,30 +10,30 @@ import scrapy
 import re
 
 
-# = Note For Other Readers
-### = Note For Me 
+### = Note For Other Readers
+# = Note For Me 
 
-#Every Class in this file represents a Spider
-#Every Spider must subclass 'scrapy.Spider'
+###Every Class in this file represents a Spider
+###Every Spider must subclass 'scrapy.Spider'
 class NewspaperSpider(scrapy.Spider):
     
-    #import to give each spider a name because you run spiders by calling them by name.
+    ###import to give each spider a name because you run spiders by calling them by name.
     name = 'newspapers_tt'
     
-    #Important thing I noticed: (See Below)
+    ###Important thing I noticed: (See Below)
     def start_requests(self):
         
         start_url = "https://en.wikipedia.org/wiki/List_of_newspapers_in_the_United_States"
-        #Only yielded pages are downloaded (i.e. parseable)
+        ###Only yielded pages are downloaded (i.e. parseable)
         yield scrapy.Request(url=start_url, callback=self.parse1)
     
-    #Parse the base wiki page for links to wiki of top 25 national papers 
+    ###Parse the base wiki page for links to wiki of top 25 national papers 
     def parse1(self, response):
         to_links = response.xpath('//*[@id="mw-content-text"]/div//table[3]/tr//td[2]//a/@href').extract()
         top_urls = [ response.urljoin(link) for link in to_links]
         
-        ### Relative Links: response.xpath('//*[@id="mw-content-text"]/div//table[3]/tr//td[2]//a/@href').extract()
-        ### Titles: response.xpath('//*[@id="mw-content-text"]/div//table[3]/tr//td[2]//a/@title').extract()
+        # Relative Links: response.xpath('//*[@id="mw-content-text"]/div//table[3]/tr//td[2]//a/@href').extract()
+        # Titles: response.xpath('//*[@id="mw-content-text"]/div//table[3]/tr//td[2]//a/@title').extract()
         
         for url in top_urls:
             yield scrapy.Request(url=url, callback=self.parse2)
@@ -41,7 +41,8 @@ class NewspaperSpider(scrapy.Spider):
     def parse2(self, response):
         h = response.xpath('//*[@id="firstHeading"]/i/text()').extract()[0]
         
-        ###TO-DO: Check to see if there is more than one  website link.
+        # TO-DO: Check to see if there is more than one  website link.
+        # load both, see which one has more links, then follow the one with more links
         sup_link1 = response.xpath('//table[@class="infobox vcard"]/tr[last() -1]//a/@href').extract()[0]
         sup_link2 = response.xpath('//table[@class="infobox vcard"]/tr[last()]//a/@href').extract()[0]
         
@@ -53,24 +54,39 @@ class NewspaperSpider(scrapy.Spider):
         
         yield scrapy.Request(url=site, callback=self.parse3)
         
+    #def parse2_5(self, response):
     
+    
+    # TO-DO: Seal Entry by finding best way to validly join links, Now that I know regex, use it to identify photographer patterns  
+    # If the path is more than 3 folder deep, choose another link
     def parse3(self, response):
         all_links = response.xpath('/html/body//a/@href').extract()
         sports_pages = [link for link in all_links if re.search('[Ss]ports', link) is not None]
+        
         p_title = response.xpath('//title/text()').extract()[0]
-        #Not Necessarily true, but is a pretty safe assumption.
+        
+        
+        ### Not Necessarily true, but is a pretty safe assumption.
         sports_home = sports_pages[0]
         
+        if '.com' not in sports_pages[0]:
+            sports_home = response.urljoin(sports_pages[0])
+        
+        yield scrapy.Request(url=sports_home, callback=self.parse4)
+        
+    
+    def parse4(self, response):
+        p_title = response.xpath('//title/text()').extract()[0]
+        
+        #list of URLs
+        articles = response.xpath('//article//a//@href').extract()
         
         yield {
-            
-            p_title : sports_home
-        
-        }
-        
-        
-
                 
+                p_title : articles
+                
+            }
+            
 
 
 # In[ ]:
@@ -81,12 +97,12 @@ class NewspaperSpider(scrapy.Spider):
 
 
 
-        ### g_url = 'https://www.google.com/search?q=' +  +'&num=100&ie=utf-8&oe=utf-8&aq=t&rls=org.mozilla:en-US:official&client=firefox-a&channel=fflb
-###         yield {
+        # g_url = 'https://www.google.com/search?q=' +  +'&num=100&ie=utf-8&oe=utf-8&aq=t&rls=org.mozilla:en-US:official&client=firefox-a&channel=fflb
+#         yield {
             
-###             h : site
+#             h : site
             
-###         }
+#         }
 
 
 
@@ -112,10 +128,46 @@ class NewspaperSpider(scrapy.Spider):
 #             Kathy Willens/Associated Press        </span>
 
 
-# In[8]:
+# In[19]:
+
+
+#Using get requests
+#Answer: How would I automate get requests?
+
+# import requests
+# import json
+# from pprint import pprint as pp
+# params = {"sort": "newest",
+#           "page": "3", 
+#           "q": "",
+#           "dom": "www.nytimes.com",
+#           "dedupe_hl": "y" }
+
+# url = "http://www.nytimes.com/svc/collections/v1/publish/www.nytimes.com/section/sports"
+# count = 0
+# urls = []
+
+# while count != 10:
+    
+#     with requests.Session() as s:
+#         s.headers.update({"User-Agent": "Mozilla/5.0 (X11; Linux x86_64)"})
+#         params['page'] = str( int(params['page']) + 1 )
+#         count += 1
+#         res = s.get(url, params=params)
+
+#         json_data = res.json()
+
+#     for i in range(len(json_data['members']['items'])):
+#         urls.append(json_data['members']['items'][i]['url'])
+    
+# #json_data
 
 
 
+
+
+    
+# urls
 
 
 # In[ ]:
--- IRP_Hand/quotes3.jl
+++ IRP_Hand/quotes3.jl
@@ -20,6 +20,8 @@
 {"    StarTribune.com: News, weather, sports from Minneapolis, St. Paul and Minnesota\n": "http://www.startribune.com/sports/"}
 {"USA TODAY: Latest World and US News  - USATODAY.com": "/sports/"}
 {"New York Post": "/sports/"}
+
+
 {"Honolulu Star-Advertiser | Hawaii News, Sports, Weather and Entertainment": "https://www.staradvertiser.com/sports/"}
 {"Newsday | Long Island's & NYC's News Source | Newsday": "http://www.newsday.com/sports"}
 {"The Wall Street Journal & Breaking News, Business, Financial and Economic News, World News and Video": "http://www.wsj.com/public/page/news-sports-scores.html"}
@@ -39,8 +41,58 @@
 {"Dallas News: Breaking News for DFW, Texas, World": "https://sportsday.dallasnews.com"}
 {"USA TODAY: Latest World and US News  - USATODAY.com": "/sports/"}
 {"New York Post": "/sports/"}
+
+
 {"Los Angeles Times - California, national and world news - Los Angeles Times": "/sports/dodgers/la-sp-dodgers-darvish-mets-20170804-story.html#nt=oft12aH-1gp2"}
 {"    StarTribune.com: News, weather, sports from Minneapolis, St. Paul and Minnesota\n": "http://www.startribune.com/sports/"}
 {"The New York Times - Breaking News, World News & Multimedia": "https://www.nytimes.com/pages/sports/index.html"}
 {"The Mercury News - Bay Area news, sports, business, entertainment, lifestyle and commentary": "http://www.mercurynews.com/sports/"}
 {"Tampa Bay, Florida news | Tampa Bay Times/St. Pete Times": "/sports/"}
+{"Cleveland OH Local News, Breaking News, Sports & Weather - cleveland.com": "http://www.cleveland.com/sports/"}
+{"Las Vegas Review-Journal \u2013 The most reliable source for Las Vegas news, breaking news": "https://www.reviewjournal.com/./sports/"}
+{"Chicago Sun-Times: Chicago news, sports, politics, entertainment": "http://chicago.suntimes.com/section/sports/"}
+{"The Wall Street Journal & Breaking News, Business, Financial and Economic News, World News and Video": "http://www.wsj.com/public/page/news-sports-scores.html"}
+{"San Diego News, California & National News - San Diego Union Tribune": "/sports/chargers/#nt=oft12aH-2li3"}
+{"Houston Chronicle | HoustonChronicle.com - Houston Chronicle": "/sports/"}
+{"New Jersey Local News, Breaking News, Sports & Weather - NJ.com": "http://www.nj.com/sports/"}
+{"Chicago Tribune: Chicago breaking news, sports, business, entertainment, weather and traffic - Chicago Tribune": "/sports/hockey/blackhawks/ct-chicago-chelsea-dagger-blackhawks-spt-0806-20170805-story.html#nt=oft08a-7gp2"}
+{"Orange County Register: Local News, Sports and Things to Do": "http://www.ocregister.com/sports/"}
+{"The Boston Globe": "//www.bostonglobe.com/sports/2016/06/20/red-sox-play-play-updates/SSxZdq5Dy0wIlzNHvUG0fN/story.html?p1=BGHeader_SmartBar_Scores_MLB"}
+{"Philadelphia local news, sports, jobs, cars, homes - Philly.com": "http://www.philly.com/philly/sports"}
+{"Newsday | Long Island's & NYC's News Source | Newsday": "http://www.newsday.com/sports"}
+{"Tampa Bay, Florida news | Tampa Bay Times/St. Pete Times": "/sports/"}
+{"The Denver Post \u2013 Colorado breaking news, sports, business, weather, entertainment.": "http://www.denverpost.com/sports/"}
+{"    StarTribune.com: News, weather, sports from Minneapolis, St. Paul and Minnesota\n": "http://www.startribune.com/sports/"}
+{"The Mercury News - Bay Area news, sports, business, entertainment, lifestyle and commentary": "http://www.mercurynews.com/sports/"}
+{"Washington Post: Breaking News, World, US, DC News & Analysis - The Washington Post": "http://www.washingtonpost.com/sports/"}
+{"Honolulu Star-Advertiser | Hawaii News, Sports, Weather and Entertainment": "https://www.staradvertiser.com/sports/"}
+{"USA TODAY: Latest World and US News  - USATODAY.com": "/sports/"}
+{"The New York Times - Breaking News, World News & Multimedia": "https://www.nytimes.com/pages/sports/index.html"}
+{"Los Angeles Times - California, national and world news - Los Angeles Times": "/sports/dodgers/la-sp-dodgers-darvish-mets-20170804-story.html#nt=oft12aH-1gp2"}
+{"New York Post": "/sports/"}
+{"Dallas News: Breaking News for DFW, Texas, World": "https://sportsday.dallasnews.com"}
+{"azcentral.com:  Arizona news, Phoenix local news, sports, entertainment, national and world news": "/sports/"}
+{"Philadelphia local news, sports, jobs, cars, homes - Philly.com": "http://www.philly.com/philly/sports"}
+{"Cleveland OH Local News, Breaking News, Sports & Weather - cleveland.com": "http://www.cleveland.com/sports/"}
+{"The Wall Street Journal & Breaking News, Business, Financial and Economic News, World News and Video": "http://www.wsj.com/public/page/news-sports-scores.html"}
+{"USA TODAY: Latest World and US News  - USATODAY.com": "https://www.usatoday.com/sports/"}
+{"The New York Times - Breaking News, World News & Multimedia": "https://www.nytimes.com/pages/sports/index.html"}
+{"    StarTribune.com: News, weather, sports from Minneapolis, St. Paul and Minnesota\n": "http://www.startribune.com/sports/"}
+{"Las Vegas Review-Journal \u2013 The most reliable source for Las Vegas news, breaking news": "https://www.reviewjournal.com/./sports/"}
+{"Honolulu Star-Advertiser | Hawaii News, Sports, Weather and Entertainment": "https://www.staradvertiser.com/sports/"}
+{"The Boston Globe": "https://www.bostonglobe.com/sports?p1=BGHeader_MainNav"}
+{"San Diego News, California & National News - San Diego Union Tribune": "http://www.sandiegouniontribune.com/sports/#nt=oft12aH-2li3"}
+{"Newsday | Long Island's & NYC's News Source | Newsday": "http://www.newsday.com/sports"}
+{"Orange County Register: Local News, Sports and Things to Do": "http://www.ocregister.com/sports/"}
+{"New Jersey Local News, Breaking News, Sports & Weather - NJ.com": "http://www.nj.com/sports/"}
+{"azcentral.com:  Arizona news, Phoenix local news, sports, entertainment, national and world news": "http://www.azcentral.com/sports/"}
+{"Los Angeles Times - California, national and world news - Los Angeles Times": "http://www.latimes.com/sports/la-sp-dodgers-darvish-hernandez-20170804-story.html#nt=oft12aH-1gp2"}
+{"Houston Chronicle | HoustonChronicle.com - Houston Chronicle": "http://www.houstonchronicle.com/sports/"}
+{"Tampa Bay, Florida news | Tampa Bay Times/St. Pete Times": "http://www.tampabay.com/sports/"}
+{"The Mercury News - Bay Area news, sports, business, entertainment, lifestyle and commentary": "http://www.mercurynews.com/sports/"}
+{"Washington Post: Breaking News, World, US, DC News & Analysis - The Washington Post": "http://www.washingtonpost.com/sports/"}
+{"Chicago Sun-Times: Chicago news, sports, politics, entertainment": "http://chicago.suntimes.com/section/sports/"}
+{"Chicago Tribune: Chicago breaking news, sports, business, entertainment, weather and traffic - Chicago Tribune": "http://www.chicagotribune.com/sports/football/bears/ct-zach-miller-inspires-young-patient-bears-spt-0806-20170805-story.html#nt=oft06a-3la1"}
+{"The Denver Post \u2013 Colorado breaking news, sports, business, weather, entertainment.": "http://www.denverpost.com/sports/"}
+{"New York Post": "http://nypost.com/sports/"}
+{"Dallas News: Breaking News for DFW, Texas, World": "https://sportsday.dallasnews.com"}
